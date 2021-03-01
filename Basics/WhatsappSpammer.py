b6e5d6295ef60ea51d787cc8b7f5052d879ea18f
Pranav Viswanathan <72350161+PranavViswanathan@users.noreply.github.com>
2021-03-01 10:57:53 +0530

    Create WhatsappSpammer.py

---
+++ Basics/WhatsappSpammer.py
@@ -0,0 +1,24 @@
+#Send text in Whatsappp web
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import random
+import win32api, win32con
+n = int(input("Number of times to spam : "))
+for i in range(0, n):
+    pyautogui.moveTo(960, 970) 
+
+    click(960, 970)
+    pyautogui.write('lol')
+
+    pyautogui.moveTo(1790, 970)
+    click(1790, 970)
+
+    pyautogui.moveTo(960, 540)
+
+
+def click(x,y):
+    win32api.SetCursorPos((2533,454))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
